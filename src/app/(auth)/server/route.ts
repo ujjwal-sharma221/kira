cf8db923f5790113cd6d60cdedb8b802f32f71f7
Ujjwal Sharma <ujjwalshrm2@gmail.com>
2024-10-19 01:38:30 +0530

    appwrite setup

--- src/app/(auth)/server/route.ts
+++ src/app/(auth)/server/route.ts
@@ -1,18 +1,49 @@
 import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
+import { ID } from "node-appwrite";
+import { deleteCookie, setCookie } from "hono/cookie";
 
 import { loginSchema, registerSchema } from "@/lib/schemas/auth-schema";
+import { createAdminClient } from "@/lib/appwrite";
+import { AUTH_COOKIE } from "@/lib/constants";
 
 const app = new Hono()
-  .post("/login", zValidator("json", loginSchema), (c) => {
+  .post("/login", zValidator("json", loginSchema), async (c) => {
     const { email, password } = c.req.valid("json");
-    console.log({ email, password });
+    const { account } = await createAdminClient();
+
+    const session = await account.createEmailPasswordSession(email, password);
+    setCookie(c, AUTH_COOKIE, session.secret, {
+      path: "/",
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 60 * 60 * 24 * 30,
+    });
 
     return c.json({ message: "success" });
   })
-  .post("/register", zValidator("json", registerSchema), (c) => {
+  .post("/register", zValidator("json", registerSchema), async (c) => {
+    const { account } = await createAdminClient();
+
     const { email, password, name } = c.req.valid("json");
-    return c.json({ email, password, name });
+
+    await account.create(ID.unique(), email, password, name);
+    const session = await account.createEmailPasswordSession(email, password);
+    setCookie(c, AUTH_COOKIE, session.secret, {
+      path: "/",
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 60 * 60 * 24 * 30,
+    });
+
+    return c.json({ success: true });
+  })
+  .post("/logout", async (c) => {
+    deleteCookie(c, AUTH_COOKIE);
+
+    return c.json({ success: true });
   });
 
 export default app;
---
+++ src/lib/constants.ts
@@ -0,0 +1,1 @@
+export const AUTH_COOKIE = "KIRA_SESSION_COOKIE";
