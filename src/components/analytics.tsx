8a4a7d4c249819aae8d27691439f7b094f9d80fd
Ujjwal Sharma <ujjwalshrm2@gmail.com>
2024-11-01 21:57:03 +0530

    project analytics

--- src/components/analytics.tsx
+++ src/components/analytics.tsx
@@ -1,6 +1,7 @@
 import { ProjectAnalyticsResponse } from "@/features/projects/api/use-get-project-analytics";
-import { ScrollArea } from "./ui/scroll-area";
+import { ScrollArea, ScrollBar } from "./ui/scroll-area";
 import { AnalyticsCard } from "./analytics-card";
+import { Separator } from "./ui/separator";
 
 export function Analytics({ data }: ProjectAnalyticsResponse) {
   return (
@@ -13,8 +14,48 @@ export function Analytics({ data }: ProjectAnalyticsResponse) {
             value={data.taskCount}
             increaseValue={data.taskDifference}
           />
+          <Separator orientation="vertical" />
+        </div>
+        <div className="flex flex-1 items-center">
+          <AnalyticsCard
+            variant={data.assignedTaskDifference > 0 ? "up" : "down"}
+            title="Assigned tasks"
+            value={data.assignedTaskCount}
+            increaseValue={data.assignedTaskDifference}
+          />
+          <Separator orientation="vertical" />
+        </div>
+
+        <div className="flex flex-1 items-center">
+          <AnalyticsCard
+            variant={data.completeTaskDifference > 0 ? "up" : "down"}
+            title="Completed tasks"
+            value={data.completeTasksCount}
+            increaseValue={data.completeTaskDifference}
+          />
+          <Separator orientation="vertical" />
+        </div>
+
+        <div className="flex flex-1 items-center">
+          <AnalyticsCard
+            variant={data.overdueTaskDifference > 0 ? "up" : "down"}
+            title="Overdue tasks"
+            value={data.overdueTasksCount}
+            increaseValue={data.overdueTaskDifference}
+          />
+          <Separator orientation="vertical" />
+        </div>
+
+        <div className="flex flex-1 items-center">
+          <AnalyticsCard
+            variant={data.incompleteTaskDifference > 0 ? "up" : "down"}
+            title="Incomplete tasks"
+            value={data.incompleteTasksCount}
+            increaseValue={data.incompleteTaskDifference}
+          />
         </div>
       </div>
+      <ScrollBar orientation="horizontal" />
     </ScrollArea>
   );
 }
